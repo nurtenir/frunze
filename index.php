a9f1f023fcee99cde6c9a73daea15555aeb5f031
nurtenir <92497598+nurtenir@users.noreply.github.com>
2022-03-29 13:29:29 +0600

    Update index.php

--- index.php
+++ index.php
@@ -28,8 +28,8 @@
 
 <body>
   <div class="hero_area">
-    <div class=" hero_bgg_box">
-      <img src="images/hero-bgg.jpg" alt="">
+    <div class=" photo5339250846011078792_box">
+      <img src="images/photo5339250846011078792.jpg" alt="">
     </div>
     <!-- header section strats -->
     <header class="header_section">
